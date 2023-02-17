5f39d41a3836c0f5eea3e20bf673a053d1bd5ba0
Cenk Salfur <kestfrats@gmail.com>
2023-02-17 20:30:05 +0300

    Article Index Creation

--- WebApplication2/WebApplication2/Areas/Blog/Controllers/ArticleController.cs
+++ WebApplication2/WebApplication2/Areas/Blog/Controllers/ArticleController.cs
@@ -3,41 +3,46 @@ using WebApplication2.Models.Partial;
 using WebApplication2.Repositories.Abstract;
 using WebApplication2.Services.Abstract;
 
-namespace WebApplication2.Areas.Blog.Controllers
+namespace WebApplication2.Areas.Blog.Controllers;
+
+
+public class ArticleController : HomeController
 {
-    public class ArticleController : Controller
+    private readonly IArticleService articleService;
+        
+    public ArticleController(IArticleService articleService)
     {
-        private readonly IArticleService articleService;
-
-        public ArticleController(IArticleService articleService)
-        {
-            this.articleService = articleService;
-        }
-        public IActionResult Create(ArticleVM model)
-        {
-            articleService.Create(model);
-
-            return View();
-           
-        }
-        public IActionResult Update(ArticleVM model)
-        {
-            articleService.Update(model);
-
-            return View();
-
-        }
-        public IActionResult Delete(ArticleVM model)
-        {
-            articleService.Delete(model);
-
-            return View();
-
-        }
-        public IActionResult CreateArticle()
-        {
-            return PartialView("Partial/_CreateArticlePartial");
-        }
-      
+        this.articleService = articleService;
     }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+    public IActionResult Create(ArticleVM model)
+    {
+        articleService.Create(model);
+
+        return View();
+           
+    }
+    public IActionResult Update(ArticleVM model)
+    {
+        articleService.Update(model);
+
+        return View();
+
+    }
+    public IActionResult Delete(ArticleVM model)
+    {
+        articleService.Delete(model);
+
+        return View();
+
+    }
+    public IActionResult CreateArticle()
+    {
+        return PartialView("Partial/_CreateArticlePartial");
+    }
+      
 }
---
+++ WebApplication2/WebApplication2/Areas/Blog/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Areas.Blog.Controllers;
+
+[Area("Blog")]
+public class HomeController : Controller
+{
+    
+}
